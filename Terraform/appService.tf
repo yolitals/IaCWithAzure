da6030dcccff6062f9aaf0af7cad91f63085475b
yolitals <yolita2402@gmail.com>
2019-04-27 11:15:33 -0600

    Update appService.tf

--- Terraform/appService.tf
+++ Terraform/appService.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_app_service_plan" "test" {
-  name                = "azbcgt2019-appserviceplan"
+  name                = "azbcgt2019-appserviceplanyl"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
 
@@ -15,7 +15,7 @@ resource "azurerm_app_service_plan" "test" {
 }
 
 resource "azurerm_app_service" "test" {
-  name                = "azbcgt2019-app-service"
+  name                = "azbcgt2019-app-serviceyl"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
   app_service_plan_id = "${azurerm_app_service_plan.test.id}"
